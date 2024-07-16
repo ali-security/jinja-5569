67c805fea9a770ee7882a6ead429a1e3c9d8e339
Seal Security <devops-admin@sealsecurity.io>
2024-07-16 15:28:14 +0000

    patched 3.1.3

--- src/jinja2/filters.py
+++ src/jinja2/filters.py
@@ -248,7 +248,9 @@ def do_items(value: t.Union[t.Mapping[K, V], Undefined]) -> t.Iterator[t.Tuple[K
     yield from value.items()
 
 
-_space_re = re.compile(r"\s", flags=re.ASCII)
+# Check for characters that would move the parser state from key to value.
+# https://html.spec.whatwg.org/#attribute-name-state
+_attr_key_re = re.compile(r\"[\s/>=]\", flags=re.ASCII)
 
 
 @pass_eval_context
